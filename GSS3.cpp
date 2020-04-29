1aab148e62639b192a9c2484d0c1cdf60465835d
Madhur sengar <42689441+sengarmadhur@users.noreply.github.com>
2020-04-29 18:02:21 +0530

    Add files via upload

--- GSS3.cpp
+++ GSS3.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 #define int long long int
 using namespace std;
-const int LOW = -9999999;
+const int LOW = -999999999999;
 struct node{
     int pref,suff,sum,maxs;
 
@@ -20,6 +20,29 @@ void build(int nd,int start,int end)
 	tree[nd].suff = max(tree[2*nd +1].suff, tree[2*nd +1].sum + tree[2*nd].suff);
 	tree[nd].maxs = max(max(tree[2*nd].maxs,tree[2*nd+1].maxs), tree[2*nd].suff +tree[2*nd+1].pref);
 }
+void update(int nd,int start,int end,int idx,int val)
+{
+    if(start == end){
+        arr[idx] = val;
+        tree[nd] = {arr[idx],arr[idx],arr[idx],arr[idx]};
+    }
+    else{
+        int mid = (start+end)/2;
+        //left
+        if(start<=idx && mid>=idx){
+            update(2*nd,start,mid,idx,val);
+        }
+        //right
+        else{
+            update(2*nd+1,mid+1,end,idx,val);
+        }
+        tree[nd].sum = tree[2*nd].sum + tree[2*nd+1].sum;
+        tree[nd].pref = max(tree[2*nd].pref, tree[2*nd].sum + tree[2*nd+1].pref);
+        tree[nd].suff = max(tree[2*nd +1].suff, tree[2*nd +1].sum + tree[2*nd].suff);
+        tree[nd].maxs = max(max(tree[2*nd].maxs,tree[2*nd+1].maxs), tree[2*nd].suff +tree[2*nd+1].pref);
+    }
+}
+
 node query(int nd,int start,int end,int l,int r)
 {
 
@@ -37,20 +60,30 @@ node query(int nd,int start,int end,int l,int r)
 	res.sum = x1.sum + x2.sum;
     return res;
 }
+
 int32_t main()
 {
-    ifstream fin;
-    fin.open("in.in");
-    int N,q; fin>>N;
 
-    for(int i=0;i<N;i++)fin>>arr[i];
+    int N,q; cin>>N;
+
+    for(int i=0;i<N;i++)cin>>arr[i];
     build(1,0,N-1);
 
-    fin>>q;
+    cin>>q;
     while(q--)
     {
-        int l,r; fin>>l>>r; l--,r--;
-        node ans =query(1,0,N-1,l,r);
-         cout<<max(max(ans.pref, ans.suff), max(ans.sum, ans.maxs))<<endl;
+        int z; cin>>z;
+         if(z == 1)
+         {
+             int l,r; cin>>l>>r; l--,r--;
+             node ans =query(1,0,N-1,l,r);
+                cout<<max(max(ans.pref, ans.suff), max(ans.sum, ans.maxs))<<endl;
+        }
+         else{
+            int x,y; cin>>x>>y;
+            x--;
+            update(1,0,N-1,x,y);
+         }
     }
 }
+
